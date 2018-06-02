148c66be55e18e43b33efd8a4b68bece376eccb0
Soheil Alizadeh <soheil-alizadeh@outlook.com>
2018-06-02 10:51:26 +0430

    add number extensions, currently just

---
+++ System.Soli/Extensions/NumberExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace System.Soli.Extensions
+{
+    public static class NumberExtensions
+    {
+        public static string ToToman(this int? o) => InternalToToman(o);
+
+        public static string ToToman(this int o) => InternalToToman(o);
+
+        public static string ToToman(this long? o) => InternalToToman(o);
+
+        public static string ToToman(this long o) => InternalToToman(o);
+
+        private static string InternalToToman(this long? o)
+        {
+            return o == null ? "0 تومان" : $"{o:#,0} تومان";
+        }                   
+    }
+}
