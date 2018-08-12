dd94176893fd549250a82016d3ed17f9bcdb9609
dev.soheilalizadeh <dev.soheilalizadeh@gmail.com>
2018-08-12 09:18:34 +0430

    add debug extensions

---
+++ System.Soli/Extensions/DebugExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace System.Soli.Extensions
+{
+    public static class DebugExtensions
+    {
+        /// <summary>
+        /// This Method check you application mode is debug or not.
+        /// <example>
+        ///  <code>
+        ///   bool isDebug = this.IsDebug(); // true
+        ///   bool isDebug = this.IsDebug(); // false
+        /// </code>
+        /// </example>
+        /// </summary>
+        /// <param name="o">The object that's unused just for prepare a extension</param>
+        /// <returns></returns>
+        public static bool IsDebug(this object o)
+        {
+#if DEBUG
+            return true;
+#else
+            return false;
+#endif  
+        }
+    }
+}   
